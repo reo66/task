675dc25b06b116bd934e7470f0d21b114c002262
EC2 Default User <ec2-user@ip-172-31-1-59.ap-northeast-1.compute.internal>
2020-07-02 04:25:10 +0000

    C5.6修了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,35 @@
 class TasksController < ApplicationController
+  
+  
+  def index
+    @user = User.find(params[:user_id])
+    @tasks = @user.tasks
+  end
+  
+  def new
+    @task = Task.new
+    @user = User.find(params[:user_id])
+  end
+  
+  def create
+     @user = User.find(params[:user_id])
+     @task = @user.tasks.new(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url
+    else
+      render :new
+    end
+  end
+  
+  private
+
+    def task_params
+      #params.require(:user).permit(:name, :description)
+      params.require(:task).permit(:name, :description)
+    end
+    
 end
+
+  
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   post   '/logout', to: 'sessions#destroy'
-  
   resources :users do
     resources :tasks
   end
