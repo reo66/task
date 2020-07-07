390b30d0fdc24de338a8fdd530a96d2dc5e53c47
EC2 Default User <ec2-user@ip-172-31-1-59.ap-northeast-1.compute.internal>
2020-07-07 14:41:59 +0000

    E1修了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,15 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
+def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+end
+     
+    
+    
     def set_user
       @user = User.find(params[:id])
     end
