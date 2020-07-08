fb1310de868442702972a149c5b89e5b4e8cac97
EC2 Default User <ec2-user@ip-172-31-1-59.ap-northeast-1.compute.internal>
2020-07-08 02:49:22 +0000

    E4修了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,7 +69,15 @@ end
     def set_user
       @user = User.find(params[:id])
     end
-    
+
+def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+end
+
      def correct_user
       redirect_to(root_url) unless current_user?(@user)
      end
