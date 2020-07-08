78e51fa8fddb2b3daf2b17d5e12cafd9c3a5c267
EC2 Default User <ec2-user@ip-172-31-1-59.ap-northeast-1.compute.internal>
2020-07-08 04:09:37 +0000

    E4修了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -82,9 +82,18 @@ end
       redirect_to(root_url) unless current_user?(@user)
      end
 
-    # システム管理権限所有かどうか判定します。
+    
     def admin_user
       redirect_to root_url unless current_user.admin?
+    end
+    
+    
+    def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end  
     end
 end
 
